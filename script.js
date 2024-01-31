12e2941c01c18367120e174c1b7f513d5d6e90e3
Bartosz Szar <szarbartosz@gmail.com>
2024-02-01 00:45:33 +0100

    add implementation of redirecting to cooked.wiki

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-alert("hello world!");
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  const URL = tabs[0].url;
+  chrome.tabs.create({ url: `https://cooked.wiki/${URL}` });
+});
